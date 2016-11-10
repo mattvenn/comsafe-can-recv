e16b32296e2687baf13d009c44c5f504b4f6756d
matt venn <matt@mattvenn.net>
2016-11-10 14:14:19 +0100

    threaded reader and writer

--- recv.py
+++ recv.py
@@ -1,13 +1,18 @@
 from pyusbtin.usbtin import USBtin
+import re
+import os
 import struct
 from pyusbtin.canmessage import CANMessage
-from time import sleep
+import time
+import threading
 
 pressure_id = 512
 flap_id = 520
 pressure_fmt = '<H'
 pressure = 0
 flap = 0
+status_id = 500
+status_fmt = '<HH'
 
 def log_data(msg):
     global pressure
@@ -20,18 +25,73 @@ def log_data(msg):
         print("flap",  msg.get_data())
         flap = 0
 
+"""
 print("starting can message listener")
 usbtin = USBtin()
-usbtin.connect("/dev/ttyACM1")
+usbtin.connect("/dev/ttyACM0")
 usbtin.add_message_listener(log_data)
 usbtin.open_can_channel(500000, USBtin.ACTIVE)
+"""
 
-print("starting pipe writer loop")
+"""
+everytime a read is made on the named pipe, write the current pressure value
+"""
+class PressureWriter(threading.Thread):
+
+    def __init__(self):
+        super(PressureWriter, self).__init__()
+        self.named_pipe = '/tmp/pressure'
+        try:
+            os.mkfifo(self.named_pipe)
+        except OSError: # file exists
+            pass
+
+    def run(self):
+        print("starting pipe writer loop")
+        while True:
+            try:
+                with open(self.named_pipe, 'w') as fh:
+                    fh.write(str(pressure))
+            except IOError: # if reader has closed before/during write then get a pipe error
+                print("ioerror")
+            time.sleep(0.01) # wait a bit to make sure that the reader has closed the pipe
+
+""" 
+monitor the alarm named pipe, and send can messages for status
+"""
+class AlarmReader(threading.Thread):
+
+    def __init__(self):
+        super(AlarmReader, self).__init__()
+        self.named_pipe = '/tmp/alarm'
+        try:
+            os.mkfifo(self.named_pipe)
+        except OSError: # file exists
+            pass
+
+    def run(self):
+        print("starting alarm monitor")
+        while True:
+            try:
+                with open(self.named_pipe, 'r') as fh:
+                    for alarm_status in fh.readlines():
+                        print(alarm_status)
+                        m = re.search('alarm:(\d+) severity:(\d+)', alarm_status)
+                        if m is not None:
+                            alarm = int(m.group(1))
+                            severity = int(m.group(2))
+                            print(alarm, severity)
+                            can_msg = struct.pack(status_fmt, alarm, severity)
+                            usbtin.send(CANMessage(status_id, can_msg)
+            except IOError: # if writer has closed before/during write then get a pipe error
+                print("ioerror")
+
+thread = PressureWriter()
+thread.daemon = True
+thread.start()
+thread = AlarmReader()
+thread.daemon = True
+thread.start()
+
 while True:
-    try:
-        with open('pressure', 'w') as fh:
-            fh.write(str(pressure))
-    except IOError: # if reader has closed before/during write then get a pipe error
-        print("ioerror")
-    sleep(0.01) # wait a bit to make sure that the reader has closed the pipe
-
+    time.sleep(1)
